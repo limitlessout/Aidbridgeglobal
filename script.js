d9d22c4a0be2b88326ce050d4d49d020fb57027e
limitlessout <limitlessout@gmail.com>
2026-05-25 17:59:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navbar = document.querySelector("nav");
+const navLinks = document.querySelectorAll("nav a");
 
-menuToggle.onclick = function () {
+/* OUVERTURE MENU */
+
+menuToggle.addEventListener("click", (e) => {
+
+  e.stopPropagation();
 
   navbar.classList.toggle("active");
 
-};
+});
+
+/* FERMETURE APRÈS CLIC */
+
+navLinks.forEach(link => {
+
+  link.addEventListener("click", () => {
+
+    navbar.classList.remove("active");
+
+  });
+
+});
+
+/* FERMETURE SI CLIC À L'EXTÉRIEUR */
+
+document.addEventListener("click", (e) => {
+
+  if(
+    !navbar.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ){
+
+    navbar.classList.remove("active");
+
+  }
+
+});
