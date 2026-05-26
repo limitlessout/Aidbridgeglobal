1afdeb59a09abeb474cbcba8ace1b9383eb2fb95
limitlessout <limitlessout@gmail.com>
2026-05-26 07:41:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -176,3 +176,36 @@ if(form){
   });
 
 }
+// =========================
+// SCROLL REVEAL SAFE
+// =========================
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+  reveals.forEach((element) => {
+
+    const windowHeight = window.innerHeight;
+
+    const revealTop =
+      element.getBoundingClientRect().top;
+
+    const revealPoint = 100;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      element.classList.add("active");
+
+    }
+
+  });
+
+}
+
+window.addEventListener(
+  "scroll",
+  revealOnScroll
+);
+
+revealOnScroll();
